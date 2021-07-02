af8832917a172b7cb961a00ceb52c9cec9a0f864
estrel1 <emilia.strelcenia@sainsburys.co.uk>
2021-07-02 09:56:07 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# LearningSiteTest
+# StepUI Cypress Test
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+[Cypress](https://cypress.io) is JavaScript End to End Testing Framework.
+Fast, easy and reliable testing for anything that runs in a browser.
+
+
+## System requirements
+### Operating System
+
+Cypress is a desktop application that is installed on your computer. The desktop application supports these operating systems:
+- macOS 10.9 and above (64-bit only)
+- Linux Ubuntu 12.04 and above, Fedora 21 and Debian 8 (64-bit only)
+- Windows 7 and above
+
+### Node.js
+If you are using npm to install Cypress, we support:
+ - Node.js 12 or 14 and above
+
+### Linux
+If you are using Linux, you'll want to have the required dependencies installed on your system.
+##### Ubuntu/Debian
+```sh
+apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+```
+##### CentOS
+```sh
+yum install -y xorg-x11-server-Xvfb gtk2-devel gtk3-devel libnotify-devel GConf2 nss libXScrnSaver alsa-lib
+```
+##### Docker
+Docker images with all of the required dependencies installed are available under [cypress/base](https://github.com/cypress-io/cypress-docker-images)
+## Installation
+
+Cypress Test Framework requires [Node.js](https://nodejs.org/) v10+ to run.
+
+Install Cypress via [npm]():
+```sh
+cd /your/project/path
+npm install cypress --save -dev
+```
+This will install Cypress locally as a dev dependency for your project.
+
+Install Cypress via [yarn]():
+```sh
+cd /your/project/path
+yarn add cypress --dev
+```
+
+## Opening Cypress
+If you used [npm]() to install, Cypress has now been installed to your [./node_modules]() directory, with its binary executable accessible from [./node_modules/.bin.]()
+```sh
+npm run e2e
+```
+or
+```sh
+./node_modules/.bin/cypress open
+```
+or
+```sh
+npx cypress open
+```
+or
+```sh
+yarn run cypress open
+```
+
+
+## License
+
+MIT
---
+++ cypress/integration/who_is_to_blame.spec.js
@@ -0,0 +1,153 @@
+describe("Finding The Truth", () => {
+    it("Access Site", () => {
+        cy.visit('https://learning.elucidat.com/course/5c9126fd760e5-60ba4c3fe8135')
+        cy.wait(8000)
+        cy.get('h1').should('contain.text', 'FINDING THE TRUTH')
+        cy.get("a.button.button--nav").should('contain.text', 'START').click({force: true})
+        cy.wait(3000)
+        
+    })
+    it("Who is to blame?", () => {
+        // cy.get('h2').should('contain.text', 'FINDING THE TRUTH')
+        cy.get("a.link.card.imageCard").eq(1).click({force: true})
+        cy.wait(3000)
+        cy.get("a.button.button--nav").should('include.text', 'JUDGE THIS').click({force: true})
+        cy.wait(3000)
+        cy.get('span.icon.icon--secondary > i').eq(0).click({force: true})
+        cy.log('Guilty is selected')
+        cy.get('a.save_button').should('include.text', 'VOTE').click({force: true})
+        cy.get('.modal__container').should('include.text', 'GUILTY!')
+        cy.get('.modal__container').should('include.text', 'You think Kevin is guilty.')
+        cy.get('.modal__container').should('include.text', 'Move on to find out more about the evidence. Will you be able to use it to prove his guilt?')
+        cy.get('.button.button--nav').eq(2).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+    it("Exploer the evidence", () => {
+        cy.get('h2.page__title').should('have.text', 'EXPLORE THE EVIDENCE')
+        // cy.get('strong').eq(0).should('include.text', "You saw how much evidence there is against Kevin. Let's take a closer look at this.")
+        cy.log('Check hamburg menu')
+        cy.get('a.menuButton').click({force: true})
+        cy.wait(5000)
+        cy.get('li.menu__item').eq(0).should('have.class', 'visited')
+        cy.get('li.menu__item').eq(0).should('include.text', 'FINDING THE TRUTH')
+        cy.get('body').click({force: true})
+        cy.get('a.button.e-button--link.e-button--medium.e-icon--after.button--nav').should('include.text', 'CONTINUE').click({force: true})
+        
+    })
+
+    it("YOU DECIDE", () => {
+        // cy.get('h2.page__title').should('include.text', 'YOU DECIDE')
+        // cy.get('p').eq(0).invoke(some_text => {cy.log(some_text)})
+        // cy.get('p').eq(1).invoke(some_text => {cy.log(some_text)})
+        // cy.get('p').eq(2).invoke(some_text => {cy.log(some_text)})
+        cy.wait(3000)
+        cy.get('a.button.e-button--link.e-button--medium.e-icon--after.button--nav').should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(5000)
+    })
+
+    it("THE FINGERPRINT", () => {
+        cy.get("h2.page__title").should('include.text', "THE FINGERPRINT")
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(0).should('include.text', 'Flip').click({force: true})
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(1).should('include.text', 'Flip').click({force: true})
+        cy.get('i.ti-answer').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.wait(2000)
+        cy.get('.modal__title').eq(1).should('include.text', 'Our expert disagrees...')
+        cy.get('.modal__container .button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+
+    it("THE DNA", () => {
+        cy.get("h2.page__title").should('include.text', "THE DNA")
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(0).should('include.text', 'Flip').click({force: true})
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(1).should('include.text', 'Flip').click({force: true})
+        cy.get('i.ti-answer').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.wait(2000)
+        cy.get('.modal__title').eq(1).should('include.text', 'Our expert does not agree')
+        cy.get('.modal__container .button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+
+    it("THE HAIR SAMPLE", () => {
+        cy.get("h2.page__title").should('include.text', "THE HAIR SAMPLE")
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(0).should('include.text', 'Flip').click({force: true})
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(1).should('include.text', 'Flip').click({force: true})
+        cy.get('i.ti-answer').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.wait(2000)
+        cy.get('.modal__title').eq(1).should('include.text', 'Our expert agrees, however...')
+        cy.get('.modal__container .button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+
+    it("THE FOOTPRINT", () => {
+        cy.get("h2.page__title").should('include.text', "THE FOOTPRINT")
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(0).should('include.text', 'Flip').click({force: true})
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(1).should('include.text', 'Flip').click({force: true})
+        cy.get('i.ti-answer').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.wait(2000)
+        cy.get('.modal__title').eq(1).should('include.text', 'Our expert agrees...')
+        cy.get('.modal__container .button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+
+    it("THE CONFESSION", () => {
+        cy.get("h2.page__title").should('include.text', "THE CONFESSION")
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(0).should('include.text', 'Flip').click({force: true})
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(1).should('include.text', 'Flip').click({force: true})
+        cy.get('i.ti-answer').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.wait(2000)
+        cy.get('.modal__title').eq(1).should('include.text', 'Our expert does not agree')
+        cy.get('.modal__container .button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+
+    it("THE EYEWITNESS", () => {
+        cy.get("h2.page__title").should('include.text', "THE EYEWITNESS")
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(0).should('include.text', 'Flip').click({force: true})
+        cy.get('a.button.button--primary.e-button--medium.e-icon--after').eq(1).should('include.text', 'Flip').click({force: true})
+        cy.get('i.ti-answer').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.wait(2000)
+        cy.get('.modal__title').eq(1).should('include.text', 'Our expert agrees...')
+        cy.get('.modal__container .button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+
+    it("Guilty or Not Guilty Again", () => {
+        cy.get('div.htmlText').eq(0).should('include.text', 'Have you changed your mind about whether Kevin is guilty or not?')
+        cy.log('Guilty Selected')
+        cy.get('i.ti.ti-answer.ti-radio').eq(0).click({force: true})
+        cy.get('i.ti.ti-link-external').click({force: true})
+        cy.get('.modal__container h2').should('include.text', 'GUILTY!')
+        cy.get('.modal__container p').eq(2).should('include.text', 'You think Kevin is guilty.')
+        cy.get('.modal__container a.button.button--nav').eq(1).should('include.text', 'CONTINUE').click({force: true})
+        cy.wait(3000)
+    })
+    
+    it("Click Final Verdict", () => {
+        cy.get('a.button.button--nav').should('include.text', 'FINAL VERDICT').click({force: true})
+        cy.wait(3000)
+    })
+
+    it('FINAL VERDICT', () => {
+        cy.get("h2.page__title").should('include.text', "FINAL VERDICT")
+        cy.get("a.button.button--nav").eq(0).should('include.text', 'WHAT THE RESEARCH SAYS').click({force: true})
+        cy.wait(3000)
+    })
+
+    it('WHAT THE RESEARCH SAYS', () => {
+        cy.get("h2.page__title").should('include.text', "WHAT THE RESEARCH SAYS")
+        cy.get("i.ti.ti-arrow-left").click({force: true})
+        cy.wait(3000)
+    })
+
+    it('FINAL VERDICT', () => {
+        cy.get("h2.page__title").should('include.text', "FINAL VERDICT")
+        cy.get("a.button.button--nav").eq(1).should('include.text', 'CHOOSE NEW CASE').click({force: true})
+        cy.wait(3000)
+    })
+})
